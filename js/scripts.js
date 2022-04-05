31f11fb6fce0c21485c0ec308e3e91239347f881
Fernando Berrospi <fberrosp@gmail.com>
2022-04-05 18:18:48 -0500

    Changed layout and added some resume data

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
-/*!
-* Start Bootstrap - Grayscale v7.0.4 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+// Scripts 
 
 window.addEventListener('DOMContentLoaded', event => {
 
